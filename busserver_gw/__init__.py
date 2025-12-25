c1c2a04acc9fbdf53b5a210ce134a16ab51eb294
xssfox <xss@sprocketfox.io>
2025-12-25 17:07:28 +1100

    initial bus server

---
+++ busserver_gw/__init__.py
@@ -0,0 +1,282 @@
+import socket
+import struct
+from dataclasses import dataclass
+import logging
+from enum import Enum
+import types
+from paho.mqtt import client as mqtt_client
+import argparse
+import random
+import time
+import json
+
+
+class MqttClient():
+    def __init__(self, broker, port, username, password, entrance_callback, lift_callback, remote_callback, unlock_callback, resident_address, entrance, lifts, remote_entrance):
+        self.entrance_callback = entrance_callback
+        self.lift_callback = lift_callback
+        self.remote_callback = remote_callback
+        self.unlock_callback = unlock_callback
+        self.connect_mqtt(broker, port, username, password)
+        self.client.loop_start()
+        self.resident_address = resident_address
+        self.entrance = entrance
+        self.remote_entrance = remote_entrance
+        self.lifts = lifts
+    def connect_mqtt(self, broker, port, username, password):
+        def on_connect(client, userdata, flags, rc):
+            if rc == 0:
+                logging.debug("Connected to MQTT Broker!")
+                self.send_discovery() # TODO send often
+                client.subscribe(f"aiphone-{self.resident_address}/command")
+            else:
+                logging.debug("Failed to connect, return code %d\n", rc)
+        # Set Connecting Client ID
+        client_id = f'aiphone-mqtt-{random.randint(0, 1000)}'
+        self.client = mqtt_client.Client(client_id)
+
+
+        self.client.username_pw_set(username, password)
+        self.client.on_connect = on_connect
+        self.client.on_disconnect = self.on_disconnect
+        self.client.on_message = self.on_message
+
+        self.client.connect(broker, port)
+        
+        logging.debug("MQTT connect called")
+
+    def on_message(self, client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        if msg.topic == f"aiphone-{self.resident_address}/command":
+            data = json.loads(msg.payload.decode())
+            if data['type'] == "entrance":
+                self.entrance_callback(int(data['entrance']))
+            if data['type'] == "lift":
+                self.lift_callback(int(data['lift']))
+            if data['type'] == "remote":
+                self.remote_callback(int(data['section']),int(data['entrance']))
+            if data['type'] == 'unlock':
+                self.unlock_callback()
+
+
+    def send_update(self, lineinuse):
+        TOPIC = f"aiphone-{self.resident_address}/state"
+        self.client.publish(TOPIC,json.dumps(
+            {
+                "line": "inuse" if lineinuse else "free"
+            }
+        ))
+    def send_ring(self,from_type, from_address, to_type, to_address):
+        TOPIC = f"aiphone-{self.resident_address}/event"
+        self.client.publish(TOPIC,json.dumps(
+            {
+                "event_type": "ring",
+                "to_type": to_type,
+                "from_type": from_type,
+                "from_address": from_address,
+                "to_address": to_address
+            }
+        ))
+    def send_discovery(self):
+        TOPIC = f"homeassistant/device/aiphone-{self.resident_address}/config"
+        config = {
+                "dev": {
+                    "ids": f"aiphone-{self.resident_address}",
+                    "name": "Aiphone Intercom",
+                    "mf": "Aiphone",
+                    "mdl": "GT 1C7W",
+                    "sw": "1.0",
+                    "sn": "",
+                    "hw": ""
+                },
+                "o": {
+                    "name":"busserver",
+                    "sw": "0.1",
+                    "url": "https://github.com/xssfox"
+                },
+                "cmps": {
+                    "lineinuse": {
+                        "p": "binary_sensor",
+                        "unique_id":"lineinuse",
+                        "name": "Line In Use",
+                        "payload_on": "inuse",
+                        "payload_off": "free",
+                        "value_template":"{{ value_json.line }}",
+                    },
+                    "call": {
+                        "p": "event",
+                        "device_class": "doorbell",
+                        "unique_id":"incoming_call",
+                        "name": "Incoming Call",
+                        "event_types": ["ring"],
+                        "state_topic": f"aiphone-{self.resident_address}/event"
+                    },
+ 
+                    "unlock" : {
+                        "p": "button",
+                        "unique_id":f"unlock_call",
+                        "name": f"Unlock call in progress",
+                        "payload_press": json.dumps({
+                            "type": "unlock"
+                        }),
+                        "command_topic": f"aiphone-{self.resident_address}/command"
+                    
+                    },
+                },
+                "state_topic":f"aiphone-{self.resident_address}/state",
+                "qos": 2
+            }
+        for entrance in self.entrance:
+            config["cmps"][f"entrance_{entrance}"] = {
+                "p": "button",
+                "unique_id":f"entrance_{entrance}",
+                "name": f"Entrance {entrance} Unlock",
+                "payload_press": json.dumps({
+                    "type": "entrance",
+                    "entrance": f"{entrance}"
+                }),
+                "command_topic": f"aiphone-{self.resident_address}/command"
+            }
+        for lifts in self.lifts:
+            config["cmps"][f"lift_{lifts}"] = {
+                "p": "button",
+                "unique_id":f"lift_{lifts}",
+                "name": f"Lift {lifts} Unlock",
+                "payload_press": json.dumps({
+                    "type": "lift",
+                    "lift": f"{lifts}"
+                }),
+                "command_topic": f"aiphone-{self.resident_address}/command"
+            }
+        for remote_entrance in self.remote_entrance:
+            config["cmps"][f"remote_{remote_entrance}"] = {
+                "p": "button",
+                "unique_id":f"remote_{remote_entrance}",
+                "name": f"Entrance {remote_entrance} Unlock",
+                "payload_press": json.dumps({
+                    "type": "remote",
+                    "section": f"{remote_entrance.split(':')[0]}",
+                    "entrance": f"{remote_entrance.split(':')[1]}",
+                }),
+                "command_topic": f"aiphone-{self.resident_address}/command"
+            }            
+        self.client.publish(TOPIC,json.dumps(config),2,True)
+    def on_disconnect(self, userdata, rc):
+        logging.error("Disconnected from MQTT")
+        while 1:
+            try:
+                self.client.reconnect()
+            except Exception as err:
+                logging.error("%s. Reconnect failed. Retrying...", err)
+            time.sleep(1)
+
+    
+
+class AddressType(Enum):
+    # POSSIBLY others for the second trunk line
+    BROADCAST_CLIENT = 0x0
+    UNKNOWN_1        = 0x1
+    UNKNOWN_2        = 0x2
+    UNKNOWN_3        = 0x3
+    UNKNOWN_4        = 0x4
+    UNKNOWN_5        = 0x5
+    UNKNOWN_6        = 0x6
+    UNKNOWN_7        = 0x7
+    RESIDENT         = 0x8
+    UNKNOWN_9        = 0x9
+    UNKNOWN_A        = 0xa
+    UNKNOWN_b        = 0xb
+    ENTRANCE         = 0xc # Can also be the gateway when address = 0x1
+    UNKNOWN_d        = 0xd
+    SECURITY         = 0xe # Can also be the gateway when address = 0x1
+    BROADCAST        = 0xf # possibly the bus expander
+
+
+COMMANDS = {
+    0x06: "ACK",
+    0x0b: "KEEPALIVE/LINEUSE?",
+    0x11: "SYSTEM_INFO",
+    0x12: "SYSTEM_INFO_2",
+    0x13: "PING?",
+    0x20: "LINE_REQUEST", # happens first
+    0x23: "LINE_REQUEST_3",
+    0x26: "LINE_REQUEST_2",
+    0x24: "HANG_UP",
+    0x29: "CALL_ESTABLISHED_1",
+    0x33: "CALL_MON", #?
+    0x35: "START_CALL_1", # from MCX?
+    0x36: "START_CALL_2", # from bus expander?
+    0x40: "CALL_ESTABLISHED_2",
+
+    0x70: 'INCOMING_CALL_RING_NO_CAM',
+    0x71: 'INCOMING_CALL_RING_CAM', # from gateway
+    0x72: 'INCOMING_CALL_RING_NO_CAM_2',
+    0x73: 'INCOMING_CALL_RING_CAM_2',
+    0x74: 'INCOMING_CALL_RING_EMERGENCY',
+    0x7b: 'INCOMING_CALL_ESTABLISHED_2', # from resident
+    0x7d: 'CALL_ESTABLISHED_3',
+    0x79: 'RING_ACK',
+
+    0x81: 'END_CALL_1',
+    0x82: 'END_CALL_2',
+    0x83: 'UNLOCK_PRESS',
+    0x84: 'UNLOCK_RELEASE',
+    0x86: 'MONITOR_REQUEST',
+    0x87: 'MONITOR_OK',
+    0x89: 'MONITOR_END',
+
+    0x9e: "REQ_TALK",
+
+    0xe1: "CAMERA_INFO",
+    0xe3: "CAMERA_INFO_2",
+    0xb0: 'LIFT_CONTROL',
+
+    0xf0: 'REMOTE_CALL_ESTABLISHED',
+    0xf1: 'REMOTE_SET_ADDRESS_1',
+    0xf2: 'REMOTE_SET_ADDRESS_2',
+    0xf3: 'REMOTE_SET_ADDRESS_3',
+    0xfb: 'REMOTE_SET_SECTION',
+}
+
+CommandType = Enum("CommandType",{ COMMANDS[x] if x in COMMANDS else f"UNKNOWN_{hex(x)}" : x for x in range(0,0xff)})
+
+
+    
+
+@dataclass
+class Packet:
+    header: int = 0
+    from_type: AddressType = 0
+    to_type: AddressType = 0
+    from_address: int = 0
+    to_address: int = 0
+    cmd: CommandType = 0
+    parameters: bytes = b''
+    def encode(self):
+        out = bytes([
+            (len(self.parameters)<<6) | self.to_type.value | self.header << 4,
+            self.to_address,
+            self.from_type.value,
+            self.from_address,
+            self.cmd.value
+        ]) + self.parameters
+        return out
+
+
+def decode(data: bytes) -> Packet:
+    out = Packet()
+    parameter_length = data[0] >> 6
+
+    out.header = data[0] >> 4
+
+    out.to_type =  AddressType(data[0] & 0xf)
+    out.to_address = data[1]
+    
+    out.from_type = AddressType(data[2] & 0xf)
+    out.from_address = data[3]
+
+    out.cmd = CommandType(data[4])
+
+    out.parameters = data[5:5+parameter_length]
+
+    return out
---
+++ busserver_gw/__main__.py
@@ -0,0 +1,187 @@
+import socket
+import struct
+from dataclasses import dataclass
+import logging
+from enum import Enum
+import types
+from paho.mqtt import client as mqtt_client
+import argparse
+import random
+import time
+import json
+from . import Packet, MqttClient, AddressType, CommandType, decode
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+
+parser = argparse.ArgumentParser(
+                    prog='busserver-gw',
+                    description='Interfaces aiphone busserver with mqtt. Note currently everything is sent as Guard 2')
+
+parser.add_argument('-b','--busserver-host', type=str, help="busserver (intercom) address", required=True)
+parser.add_argument('-m','--mqtt-host', type=str, help="MQTT Host", required=True)
+parser.add_argument('-o','--mqtt-port', type=int, help="MQTT Port", default=1883)
+parser.add_argument('-u','--mqtt-username', type=str, help="MQTT Username", required=True)
+parser.add_argument('-p','--mqtt-password', type=str, help="MQTT Password", required=True)
+parser.add_argument('-a','--resident-address', type=int, help="Resident Address", required=True)
+parser.add_argument('-e','--entrance', nargs="+", type=int, help="Simple same bus entrance IDs 1-15", default=[])
+parser.add_argument('-l','--lifts', nargs="+", type=int, help="Lift Control IDs", default=[])
+parser.add_argument('-r','--remote-entrance', nargs="+", type=str, default=[], help="Remote entrances [section:entranceid] eg: 25:1. Generally only shared zones will work (25-31)")
+
+
+args = parser.parse_args()
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+server.bind(('', 23569))
+server.settimeout(0.01)
+
+# TODO
+# - add a generic unlock button
+# - error handling parsing packets
+# - move .decode into Packet()
+
+
+def unlock_entrance(entrance_id: int):
+    logging.debug(f"unlock entrance: {entrance_id}")
+    packets = [
+        Packet(cmd=CommandType.LINE_REQUEST_2, to_type=AddressType.RESIDENT, to_address=0xf, from_type=AddressType.BROADCAST, from_address=1, header=3),
+        Packet(cmd=CommandType.REQ_TALK, to_type=AddressType.ENTRANCE, to_address=entrance_id, from_type=AddressType.BROADCAST, from_address=1),
+    ]
+    for packet in packets:
+        server.sendto(packet.encode(),(args.busserver_host, 4444))
+        time.sleep(0.1)
+    time.sleep(1)
+
+    packets = [
+        Packet(cmd=CommandType.UNLOCK_PRESS, to_type=AddressType.ENTRANCE, to_address=entrance_id, from_type=AddressType.BROADCAST, from_address=1),
+        Packet(cmd=CommandType.UNLOCK_RELEASE, to_type=AddressType.ENTRANCE, to_address=entrance_id, from_type=AddressType.BROADCAST, from_address=1),
+    ]
+    for packet in packets:
+        server.sendto(packet.encode(),(args.busserver_host, 4444))
+        time.sleep(0.2)
+    time.sleep(0.5)
+    
+    packets = [
+        Packet(cmd=CommandType.END_CALL_1, to_type=AddressType.ENTRANCE, to_address=entrance_id, from_type=AddressType.BROADCAST, from_address=1),
+        Packet(cmd=CommandType.HANG_UP, to_type=AddressType.RESIDENT, to_address=0xf, from_type=AddressType.BROADCAST, from_address=1, header=3),
+    ]
+    for packet in packets:
+        server.sendto(packet.encode(),(args.busserver_host, 4444))
+        time.sleep(0.1)
+def trigger_lift_control(lift_id: int):
+    packet = Packet(
+        to_type=AddressType.BROADCAST, 
+        to_address=1, 
+        from_type=AddressType.ENTRANCE, 
+        from_address=1, # we pretend to be the gateway here - I don't think it matters
+        cmd=CommandType.LIFT_CONTROL,
+        parameters=lift_id.to_bytes(length=2,byteorder='big')
+    ).encode()
+    server.sendto(packet,(args.busserver_host, 4444))
+
+def unlock_remote_entrance(section: int,entrance_id: int):
+    logging.debug(f"unlock remote entrance: {section} {entrance_id}")
+    packets = [
+        Packet(cmd=CommandType.REMOTE_SET_SECTION, parameters=bytes([section]), to_type=AddressType.ENTRANCE, to_address=1, from_type=AddressType.SECURITY, from_address=2),
+        Packet(cmd=CommandType.REMOTE_SET_ADDRESS_1, parameters=";;".encode(), to_type=AddressType.ENTRANCE, to_address=1, from_type=AddressType.SECURITY, from_address=2),
+        Packet(cmd=CommandType.REMOTE_SET_ADDRESS_2, parameters=";;".encode(), to_type=AddressType.ENTRANCE, to_address=1, from_type=AddressType.SECURITY, from_address=2),
+        Packet(cmd=CommandType.REMOTE_SET_ADDRESS_3, parameters=f";{entrance_id}".encode(), to_type=AddressType.ENTRANCE, to_address=1, from_type=AddressType.SECURITY, from_address=2),
+    ]
+    for packet in packets:
+        server.sendto(packet.encode(),(args.busserver_host, 4444))
+        time.sleep(0.1)
+    time.sleep(0.7)
+
+    packets = [
+        Packet(cmd=CommandType.UNLOCK_PRESS, to_type=AddressType.ENTRANCE, to_address=1, from_type=AddressType.SECURITY, from_address=2),
+        Packet(cmd=CommandType.UNLOCK_RELEASE, to_type=AddressType.ENTRANCE, to_address=1, from_type=AddressType.SECURITY, from_address=2),
+    ]
+    for packet in packets:
+        server.sendto(packet.encode(),(args.busserver_host, 4444))
+        time.sleep(0.2)
+    time.sleep(0.5)
+    
+    packets = [
+        Packet(cmd=CommandType.END_CALL_1, to_type=AddressType.ENTRANCE, to_address=1, from_type=AddressType.SECURITY, from_address=2),
+    ]
+    for packet in packets:
+        server.sendto(packet.encode(),(args.busserver_host, 4444))
+        time.sleep(0.1)
+
+last_seen_entrance = 0
+
+def unlock():
+    logging.debug(f"unlock in progress: {last_seen_entrance}")
+    packets = [
+        Packet(cmd=CommandType.UNLOCK_PRESS, to_type=AddressType.ENTRANCE, to_address=last_seen_entrance, from_type=AddressType.RESIDENT, from_address=args.resident_address),
+        Packet(cmd=CommandType.UNLOCK_RELEASE, to_type=AddressType.ENTRANCE, to_address=last_seen_entrance, from_type=AddressType.RESIDENT, from_address=args.resident_address),
+    ]
+    for packet in packets:
+        server.sendto(packet.encode(),(args.busserver_host, 4444))
+        time.sleep(0.2)
+    
+    
+
+
+mqtt = MqttClient(
+    args.mqtt_host,
+    args.mqtt_port,
+    args.mqtt_username,
+    args.mqtt_password,
+    unlock_entrance,
+    trigger_lift_control, 
+    unlock_remote_entrance, 
+    unlock,
+    args.resident_address,
+    args.entrance,
+    args.lifts,
+    args.remote_entrance
+    )
+
+CALL_TYPES = [
+    CommandType.INCOMING_CALL_RING_NO_CAM,
+    CommandType.INCOMING_CALL_RING_CAM,
+    CommandType.INCOMING_CALL_RING_NO_CAM_2,
+    CommandType.INCOMING_CALL_RING_CAM_2,
+    CommandType.INCOMING_CALL_RING_EMERGENCY
+
+]
+
+while True:
+    try:
+        message, address = server.recvfrom(24)
+        if len(message) == 24: # TODO WE CAN CHECK FOR ACKS HERE
+            message = message[13:]
+        logging.debug(message.hex())
+        packet = decode(message)
+                
+        
+        logging.debug(packet)
+        if packet.cmd in [
+            CommandType.LINE_REQUEST,
+            CommandType.START_CALL_1, 
+            CommandType.START_CALL_2, 
+            CommandType.LINE_REQUEST_3, 
+            CommandType.LINE_REQUEST_2
+        ]:
+            mqtt.send_update(True)
+        if packet.cmd == CommandType.HANG_UP:
+            mqtt.send_update(False)
+        
+        if packet.cmd in CALL_TYPES:
+            if packet.to_address == args.resident_address and packet.to_type == AddressType.RESIDENT:
+                logging.info("Detected incoming call")
+                mqtt.send_ring(
+                    packet.from_type.name,
+                    packet.from_address, 
+                    packet.to_type.name, 
+                    packet.to_address
+                )
+                last_seen_entrance = packet.from_address 
+
+
+    except TimeoutError:
+        pass
+    
